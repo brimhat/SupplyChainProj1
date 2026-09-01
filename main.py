858541a0a9325566ef645400b2e8e1b3f95c0e72
brimhat <csanders54090@gmail.com>
2026-08-31 21:04:38 -0700

    Added significance to the one-filter-analysis fn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from gurobipy import Model, GRB, LinExpr
+from scipy.stats import f_oneway
 from kaggle_install import path
 
 csv_path = path + f"\\incom2024_delay_example_dataset.csv"
@@ -38,16 +38,24 @@ def profit_analysis_by_one_category(df, ctype):
         profit_per_order = np.array(data_dict[category_type])
         sample_avg = np.mean(profit_per_order)
         sample_std = np.std(profit_per_order)
+        sample_size = len(profit_per_order)
+
+        f_statistic, p_value = f_oneway(profit_per_order, global_profit_per_order)
+        print(f_statistic, p_value)
+        if 0.05 < p_value:
+            continue
+
+        print("significant:", category_type)
         [ucl, lcl] = [sample_avg - 2*sample_std, sample_avg + 2*sample_std]
-        order_index_arr = range(len(profit_per_order))
+        order_index_arr = range(sample_size)
         plt.plot(order_index_arr, profit_per_order)
-        plt.plot(order_index_arr, [global_ucl]*len(profit_per_order), color='r', linestyle='dashed', linewidth=1)
-        plt.plot(order_index_arr, [global_lcl]*len(profit_per_order), color='r', linestyle='dashed', linewidth=1)
-        plt.plot(order_index_arr, [ucl]*len(profit_per_order), color='y', linestyle='dashed', linewidth=1)
-        plt.plot(order_index_arr, [lcl]*len(profit_per_order), color='y', linestyle='dashed', linewidth=1)
+        plt.plot(order_index_arr, [global_ucl]*sample_size, color='r', linestyle='dashed', linewidth=1)
+        plt.plot(order_index_arr, [global_lcl]*sample_size, color='r', linestyle='dashed', linewidth=1)
+        plt.plot(order_index_arr, [ucl]*sample_size, color='y', linestyle='dashed', linewidth=1)
+        plt.plot(order_index_arr, [lcl]*sample_size, color='y', linestyle='dashed', linewidth=1)
         plt.xlabel("Order number")
         plt.ylabel("Profit per order")
         plt.title("Profit per order for " + category_type)
         plt.show()
 
-profit_analysis_by_one_category(data_frame, "customer_country")
+profit_analysis_by_one_category(data_frame, "customer_city")
